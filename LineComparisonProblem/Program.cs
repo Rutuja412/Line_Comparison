cae0ba77068800bbabbba44eb2b28d8332e3d3fb
Rutuja412 <rutujakadam4545@gmail.com>
2023-01-05 15:27:40 +0530

    [Rutuja]:Adding Equality of  lines

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -7,8 +7,11 @@ namespace LineComparisonProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison");
-            LenghtOfLine lenghtOfLine = new LenghtOfLine();
-            lenghtOfLine.Calculate();
+            //LenghtOfLine lenghtOfLine = new LenghtOfLine();
+            //lenghtOfLine.Calculate();
+
+            CheckEquality checkEquality = new CheckEquality();
+            checkEquality.Equality();
         }
     }
 }
